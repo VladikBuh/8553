ff3026df7d807a40b00f1ddd80751856a2f5affa
vladikstyle <62757817+vladikstyle@users.noreply.github.com>
2026-06-26 14:35:26 +0400

    iks

--- ios/TravelPinsAmazingFinds/AppDelegate.swift
+++ ios/TravelPinsAmazingFinds/AppDelegate.swift
@@ -3,10 +3,13 @@ import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 import GoogleMaps
+import FirebaseCore
 
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
     self.moduleName = "TravelPinsAmazingFinds"
     self.dependencyProvider = RCTAppDependencyProvider()
 
